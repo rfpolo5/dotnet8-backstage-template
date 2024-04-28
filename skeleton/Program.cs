cb0c1bac341ff5daa4a3964a3e500e44d806bc64
0GiS0 <giselatb@outlook.com>
2024-04-28 22:36:29 +0200

    Refactor Program.cs to use anonymous type for WeatherForecast creation

--- skeleton/Program.cs
+++ skeleton/Program.cs
@@ -24,12 +24,12 @@ var summaries = new[]
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+        new
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Temperature = Random.Shared.Next(-20, 55),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        })
         .ToArray();
     return forecast;
 })
@@ -37,8 +37,3 @@ app.MapGet("/weatherforecast", () =>
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
